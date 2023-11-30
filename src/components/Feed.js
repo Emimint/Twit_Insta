dc480a7178bce6f54405d8551a55246ca69eec46
Emimint <90863470+Emimint@users.noreply.github.com>
2023-11-30 16:24:41 -0500

    Update Feed.js

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,16 @@
 import Thread from "./Thread";
 
-function Feed() {
+function Feed({ user, filteredThreads, setShowPopUp, getThreads }) {
   return (
-      <div className="feed">
-      <Thread />
+    <div className="feed">
+      { filteredThreads?.map( ( thread ) =>
+        <Thread
+          key={ thread.id }
+          thread={ thread }
+          user={ user }
+          setShowPopUp={ setShowPopUp } 
+          getThreads={ getThreads }
+          /> ) }
     </div>
   );
 }
