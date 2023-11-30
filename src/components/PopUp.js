e893b28cb4afe242f2ca415dde92c3c374a43fee
Emimint <90863470+Emimint@users.noreply.github.com>
2023-11-30 16:19:05 -0500

    implement PopUp component behavior

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,9 +1,10 @@
 import PopUpThread from "./PopUpThread";
 import ThreadInput from "./ThreadInput";
 
-function PopUp() {
+function PopUp({ user, setShowPopUp }) {
   return (
-      <div className="popup">
+    <div className="popup">
+      <p onClick={()=>setShowPopUp(false)}>X</p>
           <PopUpThread />
           <ThreadInput/>
     </div>
